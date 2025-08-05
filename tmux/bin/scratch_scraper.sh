d2b83a5cb779abb59edc3e5411b82ed2748e7182
Michal Trybus <komar007@gmail.com>
2025-08-05 18:43:57 +0200

    refactor(scraper): generalized PROMPT_NO_TITLE and renamed to SHELL_SESSION_RECORDER_BY
    this allows to apply various simplifications in ansi code generation on
    the shell side in case when the output is being processed by anything
    that may have problems interpreting them correctly.

--- tmux/bin/scratch_scraper.sh
+++ tmux/bin/scratch_scraper.sh
@@ -4,6 +4,17 @@
 #
 # When the shell exits, run fzf_scrape_capture.sh to let the user select
 # tokenized items from the screen capture and print the selected items.
+#
+# The shell is run with SHELL_SESSION_RECORDED_BY env set to one of the following:
+# - "script" - shell is being recorder by the "script" tool,
+# - "" (empty string or no value set) - shell is not being recorded.
+#
+# This variable is provided for the shell configuration to be able to adjust
+# the terminal features used to prevent problems with correct recording by
+# the tool.
+#
+# Known quirks that must be handled by the shell are:
+# - setting title is not correctly handled by script.
 
 set -e
 DIR=$(cd "$(dirname "$0")" && pwd)
@@ -11,7 +22,7 @@ DIR=$(cd "$(dirname "$0")" && pwd)
 RAW=$(mktemp)
 CAPTURE=$(mktemp)
 CAPTURE_BARE=$(mktemp)
-PROMPT_NO_TITLE=1 script -B "$RAW" -q -c "$SHELL" </dev/tty >/dev/tty 2>/dev/tty || true
+SHELL_SESSION_RECORDED_BY=script script -B "$RAW" -q -c "$SHELL" </dev/tty >/dev/tty 2>/dev/tty || true
 sed '1d' "$RAW" | head -n -2 > "$CAPTURE"
 "${DEP_PREFIX}ansi2txt" < "$CAPTURE" > "$CAPTURE_BARE"
 if [ "$(wc -l < "$CAPTURE_BARE")" -le 3 ]; then
