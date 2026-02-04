cd72bcba6dbc5fce8b38e77ab741a08de7d6fdb2
Michal Trybus <M.Trybus@adbglobal.com>
2026-02-04 13:00:04 +0100

    feat(scraper): add sri tokenizer (https://www.w3.org/TR/sri-2/)

--- tmux/bin/tokenize_capture.sh
+++ tmux/bin/tokenize_capture.sh
@@ -6,6 +6,7 @@ if [ "$HOW" = "--list" ]; then
     echo urls
     echo words
     echo WORDS
+    echo sri
     exit 0
 fi
 
@@ -13,6 +14,7 @@ case "$HOW" in
     urls) REGEX="https?:\/\/[^][ ^<>]+" ;;
     words) REGEX="[^][() 	]{4,}" ;;
     WORDS) REGEX="[^ 	]{4,}" ;;
+    sri) REGEX="sha(256|384|512)-[A-Za-z0-9+/=]{44,88}" ;;
 esac
 
 grep --color=none -onP "$REGEX(?!.*)" | sed 's/:/ /' | tac | awk '!seen[$0]++'
